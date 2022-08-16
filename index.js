5f84566bb29554c13d50721b90c5710114201540
Simon Ngugi Ngigi <simonngugi88@gmail.com>
2022-08-16 08:38:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+const names = ["Mary", "Shiku", "Kinami"];
+
+function writeCards(names, birthday) {
+    const newArray = [];
+    for(let i = 0; i < names.length; i++) {
+        let message =  `Thank you, ${names[i]}, for the wonderful ${birthday} gift!`;
+        newArray.push(message);
+    }
+    return newArray;
+}
+
+const countDown = i => { while(i >= 0) { console.log(i); i--; }};
